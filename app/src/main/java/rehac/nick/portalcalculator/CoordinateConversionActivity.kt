a0610476910a62a2b95d5ef2ebda93ce01b88ef3
Nicholas Rehac <tup88289@temple.edu>
2024-08-10 22:43:24 -0400

    started on CoordinateConversionActivity, created custom FFFF:FFFF:FFFF:FFFF formatted textfield

--- app/src/main/java/rehac/nick/portalcalculator/CoordinateConversionActivity.kt
+++ app/src/main/java/rehac/nick/portalcalculator/CoordinateConversionActivity.kt
@@ -1,43 +1,106 @@
 package rehac.nick.portalcalculator
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import rehac.nick.portalcalculator.ui.theme.PortalCalculatorTheme
+import kotlin.math.min
 
 class CoordinateConversionActivity : ComponentActivity() {
+    val viewModel: CoordinateConversionViewModel by lazy{ViewModelProvider(this)[CoordinateConversionViewModel::class.java]}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         overrideActivityTransition(OVERRIDE_TRANSITION_OPEN, R.anim.slide_in_right, R.anim.slide_out_left)
         overrideActivityTransition(OVERRIDE_TRANSITION_CLOSE, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
         setContent {
-            PortalCalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainInteraction()
+            MainContent(galacticAddress = viewModel.galacticAddress)
+        }
+    }
+
+    @Composable
+    fun MainContent(modifier: Modifier = Modifier, galacticAddress: MutableState<String>) {
+        PortalCalculatorTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+
+                Column {
+                    Text(
+                        "COORDINATE CONVERSION",
+                        Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        lineHeight = 50.sp,
+                        fontSize = 50.sp
+                    )
+                    Spacer(
+                        Modifier.height(50.dp)
+                    )
+                    val textFieldValue = remember {mutableStateOf(TextFieldValue())}
+                    OutlinedTextField(
+                        value = textFieldValue.value,
+                        placeholder = { Text("0123:4567:89AB:CDEF") },
+                        onValueChange = { change ->
+                            val deformatted = change.text.uppercase()
+                                .filter { it.isDigit() || (it.isLetter() && (it < 'G')) }
+                                .take(16).also {
+                                    galacticAddress.value = it
+                                }
+                            val reformatted = deformatted.let {
+                                var retval = ""
+                                for(i in it.indices) {
+                                    retval += it[i]
+                                    if((i + 1) % 4 == 0 && i != 15) retval += ":"
+                                }
+                                retval
+                            }
+                            textFieldValue.value = change.copy(
+                                reformatted,
+                                if(change.selection.start == change.text.length && change.text.length >= textFieldValue.value.text.length) TextRange(reformatted.length) else change.selection
+                            )
+                        }
+                    )
                 }
             }
         }
     }
 
-    @Composable
-    fun MainInteraction(modifier: Modifier = Modifier) {
-        Text("HEO")
-    }
-
+    @SuppressLint("UnrememberedMutableState")
     @Preview(showBackground = true)
     @Composable
-    fun MainInteractionPreview() {
-        MainInteraction()
+    fun MainContentPreview() {
+        val galacticAddress = mutableStateOf("22451234")
+        MainContent(galacticAddress = galacticAddress)
     }
 }
+
+data class CoordinateConversionViewModel(
+    val galacticAddress: MutableState<String> = mutableStateOf("")
+) : ViewModel()
